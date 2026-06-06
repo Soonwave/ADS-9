c46158c2c6f710e056fc62d5e3e26fda96a3c061
Soonwave <evawnoos@bk.ru>
2026-06-06 17:42:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include <iostream>
-#include <vector>
 #include <chrono>
+#include <cstdint>
+#include <iostream>
 #include <random>
+#include <vector>
+
 #include "tree.h"
 
 void printPerm(const std::vector<char>& v) {
@@ -24,6 +26,7 @@ int main() {
 
     printPerm(getPerm1(tree4, 2));
     printPerm(getPerm2(tree4, 2));
+
     std::random_device rd;
     std::mt19937 gen(rd());
 
@@ -35,28 +38,34 @@ int main() {
 
         PMTree expTree(test_in);
 
-        long long total_perms = 1;
+        int64_t total_perms = 1;
         for (int i = 2; i <= n; ++i) {
             total_perms *= i;
         }
 
-        std::uniform_int_distribution<long long> dist(1, total_perms);
-        long long random_num = dist(gen);
+        std::uniform_int_distribution<int64_t> dist(1, total_perms);
+        int64_t random_num = dist(gen);
 
         auto start = std::chrono::high_resolution_clock::now();
         getAllPerms(expTree);
         auto stop = std::chrono::high_resolution_clock::now();
-        auto t_all = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+        auto t_all = std::chrono::duration_cast<std::chrono::microseconds>(
+            stop - start)
+            .count();
 
         start = std::chrono::high_resolution_clock::now();
         getPerm1(expTree, static_cast<int>(random_num));
         stop = std::chrono::high_resolution_clock::now();
-        auto t_p1 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+        auto t_p1 = std::chrono::duration_cast<std::chrono::microseconds>(
+            stop - start)
+            .count();
 
         start = std::chrono::high_resolution_clock::now();
         getPerm2(expTree, static_cast<int>(random_num));
         stop = std::chrono::high_resolution_clock::now();
-        auto t_p2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+        auto t_p2 = std::chrono::duration_cast<std::chrono::microseconds>(
+            stop - start)
+            .count();
 
         std::cout << "N=" << n
             << " | getAll=" << t_all
